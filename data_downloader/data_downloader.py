3e030f4ea9d8f3dc3d451143b1ecef3117e0de4d
Nathan Gyger <ggn2@bfh.ch>
2019-03-10 20:42:55 +0100

    Add offset parameter

--- data_downloader/data_downloader.py
+++ data_downloader/data_downloader.py
@@ -7,11 +7,12 @@ import urllib.parse
 ##################################
 
 nr_images = '10'  # if you want to download all the images = 23906
+offset_imgs = '0' # if you want to start downloading from a specific image
 nr_imgs_in_zip = 3  # cannot be higher than 300
 
 
 url = 'https://isic-archive.com/api/v1/'
-get_images_and_metadata_method = 'image?limit=' + nr_images + '&sort=name&sortdir=1&detail=true'
+get_images_and_metadata_method = 'image?limit=' + nr_images + '&offset=' + offset_imgs + '&sort=name&sortdir=1&detail=true'
 
 # complete URL to get all the images ID and the tag benign/malignant
 url_request = url + get_images_and_metadata_method
