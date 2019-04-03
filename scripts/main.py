1ac4646c37a7c6c57fa9a1d246dcf7479bd37d23
Nathan Gyger <ggn2@bfh.ch>
2019-04-03 14:30:40 +0200

    Use torchvision.dataset.ImageFolder.
    Deletes dataset.py and Replace torch.utils.data.Dataset by
    torchvision.dataset.ImageFolder to import the data. Folder
    structure had to be modified by adding a "benign" and a
    "malignant" folder to "data/ISIC-images/".

--- scripts/main.py
+++ scripts/main.py
@@ -1,25 +1,41 @@
 import torch
-import torchvision
-import torchvision.transforms
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
 import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
 import matplotlib.pyplot as plt
-from dataset import MelanomaDataset
+import time
+import os
+import copy
 import util
-import glob
 
 # Directories
-metadata_path = '../data/metadata.json'
-images_path = '../data/ISIC-images/'
+data_dir = '../data/ISIC-images/'
+
+def imshow(inp, title=None):
+    if title is not None:
+        plt.title(title)
+    plt.imshow(inp)
+    plt.pause(0.001) 
 
 if __name__ == '__main__':
-
-    # Dataset loading
-    melanoma_dataset = MelanomaDataset(images_path, metadata_path)
     
-    # Testing dataset class
-    util.showSamples(melanoma_dataset)
+    image_dataset = datasets.ImageFolder(data_dir)
+    dataloader = torch.utils.data.DataLoader(image_dataset, batch_size=4,
+                                                 shuffle=True, num_workers=4)
     
+    dataset_sizes = len(image_dataset)
+    class_names = image_dataset.classes
     
-    # TODO: data augmentation for class malignant
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    
+    # NOT WORKING YET - TypeError
+#    inputs = next(iter(dataloader))
+#    out = torchvision.utils.make_grid(inputs)
+#    util.imshow(out)
+    
+    # TODO: show sample images using dataloader
     # TODO: split data into train, test and validation
-    # TODO: create dataloader
+    # TODO: data augmentation for class malignant
